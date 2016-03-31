d3a53ee06b979680c5fca157a3f8557e14c85080
ragitam_6706144126 <rachmadagitam@users.noreply.github.com>
2016-03-31 11:13:54 +0700

    Update faktorial.java

--- faktorial.java
+++ faktorial.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class faktorial {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Masukkan Angka : ");
+        System.out.print("Masukkan Angka aaaaaaaa : ");
         int n = sc.nextInt();
         for(int x=n; x>=1; x--){
             for(int y=x-1; y>=1; y--){
